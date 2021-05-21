694154aabe527e7f2af493d0509298dd5ee28016
jeantiston <jean.tiston.work@gmail.com>
2021-05-21 19:24:40 +0800

    Added copy to clipboard share link

--- frontend/src/pages/Share.js
+++ frontend/src/pages/Share.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
+import { useSelector } from 'react-redux'
+import { selectCurrentAccount } from '../reducers/accountsSlice'
+
 import styles from '../styles/share.module.css'
 
 const Share = () => {
+
+    const currentAccount = useSelector(selectCurrentAccount)
+
     return (
-        <div className={styles.sharePage} >
+        <div className={styles.sharePage} onClick={() => {navigator.clipboard.writeText(`https://gridxplan.social/planner/preview/${currentAccount.username}`)}}>
             <h2>share this link to preview and comment</h2>
-            {/* put a copy to clipboard by clicking */}
-            <p className={styles.shareLink}>https://gridxplan.social/asdiopdk</p>
+            <p>click the link to copy it to your clipboard</p>
+            <p className={styles.shareLink}>https://gridxplan.social/planner/preview/{currentAccount.username} </p>
         </div>
     )
 }
