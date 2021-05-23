68ee75f6ef382f8a5ef9931096516545473316e6
jeantiston <jean.tiston.work@gmail.com>
2021-05-24 00:17:43 +0800

    Made image upload into a button

--- frontend/src/components/BottomMenu.js
+++ frontend/src/components/BottomMenu.js
@@ -3,6 +3,7 @@ import { NavLink, useLocation } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarAlt, faTag, faFileImage, faHashtag, faShareAlt, faCog } from '@fortawesome/free-solid-svg-icons'
 
+import ImageUpload from './ImageUpload'
 import styles from '../styles/bottommenu.module.css'
 
 const BottomMenu = () => {
@@ -16,10 +17,10 @@ const BottomMenu = () => {
         //     'path': '/',
         //     'icon': faTag
         // },
-        {   'name': 'add image',
-            'path': '/add',
-            'icon': faFileImage
-        },
+        // {   'name': 'add image',
+        //     'path': '/add',
+        //     'icon': faFileImage
+        // },
         // {   'name': 'hashtags',
         //     'path': '/hashtags',
         //     'icon': faHashtag
@@ -53,11 +54,50 @@ const BottomMenu = () => {
                 </NavLink>
             )
         })
+
+    // const currentAccount = useSelector(selectCurrentAccount)
+    // const dispatch = useDispatch()
+
+    // const handleChange = (e) => {
+    //     // if (e.target.files[0].size < 1024){
+    //         console.log(e.target.files[0])
+    //         // setImage({image: e.target.files})
+    //     // }
+
+    //     let formData = new FormData()
+    //     formData.append('image', e.target.files[0])
+    //     formData.append('account', currentAccount.username)
+
+    //     fetch('/api/post/add', {
+    //         method: 'POST',
+    //         body: formData
+    //     }).then( response => response.json())
+    //     .then( response => {
+    //         console.log("response")
+    //         console.log(response)
+    //         const post = {
+    //             image: response.image,
+    //             postId: parseInt(response.id)
+    //         }
+    //         dispatch(addCell(post))
+
+    //     })
+    // }
     
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.flex}>
+                {/* <input id="image-upload" type="file" accept="image/*" onChange={handleChange} name="image" />
+                <label for="image-upload" >
+                    <div className={styles.menuItem} >
+                            <FontAwesomeIcon icon={faFileImage} size="2x" className={ styles.icon } />
+                            <sub className={styles.menuText}>add image</sub>
+                    </div>
+                </label> */}
+
+                <ImageUpload />
+
                 { menuItems }
             </div>
         </div>
---
+++ frontend/src/components/ImageUpload.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileImage } from '@fortawesome/free-solid-svg-icons'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectCurrentAccount } from '../reducers/accountsSlice'
+import { addCell } from '../reducers/gridSlice'
+
+import styles from '../styles/bottommenu.module.css'
+
+const ImageUpload = () => {
+
+    const currentAccount = useSelector(selectCurrentAccount)
+    const dispatch = useDispatch()
+
+    const handleChange = (e) => {
+        // if (e.target.files[0].size < 1024){
+            console.log(e.target.files[0])
+            // setImage({image: e.target.files})
+        // }
+
+        let formData = new FormData()
+        formData.append('image', e.target.files[0])
+        formData.append('account', currentAccount.username)
+
+        fetch('/api/post/add', {
+            method: 'POST',
+            body: formData
+        }).then( response => response.json())
+        .then( response => {
+            console.log("response")
+            console.log(response)
+            const post = {
+                image: response.image,
+                postId: parseInt(response.id)
+            }
+            dispatch(addCell(post))
+
+        })
+    }
+
+    return(
+        <div>
+            <input id="image-upload" type="file" accept="image/*" onChange={handleChange} name="image" />
+                <label for="image-upload" >
+                    <div className={styles.menuItem} >
+                            <FontAwesomeIcon icon={faFileImage} size="2x" className={ styles.icon } />
+                            <sub className={styles.menuText}>add image</sub>
+                    </div>
+                </label>
+        </div>
+    )
+}
+
+export default ImageUpload
