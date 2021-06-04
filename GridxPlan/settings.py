6ebc5b4cf414d9bb10b28dfd00ddf41aac8826bf
jeantiston <jean.tiston.work@gmail.com>
2021-06-05 01:09:39 +0800

    Heroku settings

--- GridxPlan/settings.py
+++ GridxPlan/settings.py
@@ -117,6 +117,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+import django_heroku
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
